ac616006fe1396479d1925387b75fb55f1f85f54
falkenmaze <Lightning_Falken@protonmail.com>
2025-08-03 03:38:06 -0400

    Implemented market regime detection using HMM and labeled bull/bear states

---
+++ hmm_model.py
@@ -0,0 +1,30 @@
+from hmmlearn import GaussianHMM
+import numpy as np 
+import pandas as pd 
+
+def hmm_model(log_returns, n_components=2):
+	if len(log_returns.shape) == 1:
+		log_returns = log_returns.values.reshape(-1,1)
+	model = GaussianHMM(n_components = 2, covariance_type='full')
+	model.fit(log_returns)
+	hidden_states = model.predict(log_returns)
+	return model, hidden_states
+
+def label_regimes(log_returns, hidden_states):
+	df = pd.DataFrame({
+		'return': log_returns.flatten(),
+		'state': hidden_states
+		})
+	
+	regime_stats = df.groupby('state')['return'].agg(['mean', 'std'])
+
+	sorted_sates = regime_stats.sort_values('mean').index.tolist()
+
+	regime_labels = {
+	sorted_states[0]: 'bear',
+	sorted_states[1]: 'bull'
+	}
+
+	state_series = df['state'].map(regime_labels)
+
+	return regime_labels, state_series
